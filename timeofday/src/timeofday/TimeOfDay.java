ffadd726c878392f28b3a3d2595d0c89636df941
tsmeets9 <47253276+tsmeets9@users.noreply.github.com>
2020-02-24 17:51:02 +0100

    Immutable TimeOfDay

--- timeofday/src/timeofday/TimeOfDay.java
+++ timeofday/src/timeofday/TimeOfDay.java
@@ -25,33 +25,20 @@ public class TimeOfDay {
 	}
 	
 	/**
-	 * Sets this object's hours.
-	 * 
-	 * @pre The given hours are between 0 and 23.
-	 * 		| 0<= hours && hours <= 23
-	 * @post This object's hours equal the given hours.
-	 * 		| getHours() == hours
-	 * @post This object's minutes have remained unchanged.
-	 * 		| getMinutes() == old(getMinutes())
-	 * @param hours
-	 */
-	void setHours(int hours) {
-		minutesSinceMidnight = getMinutes() + hours * 60; ;
-	}
-	
-	/**
-	 * Sets this object's minutes.
-	 * 
-	 * @pre The given minutes are between 0 and 59.
+	 * Initializes this instance with the given hours and minutes.
+	 * @pre The given hours are between 0 and 23
+	 * 		| 0 <= hours && hours <= 23
+	 * @pre The given minutes are between 0 and 59
 	 * 		| 0<= minutes && minutes <= 59
-	 * @post This object's minutes equal the given minutes.
-	 * 		| getMinutes() == minutes 
-	 * @post This object's hours have remained unchanged.
-	 * 		| getHours() == old(getHours())
+	 * @post This instance's hours equal the given hours.
+	 * 		|getHours() == hours
+	 * @post This instance's minutes equal the given minutes.
+	 * 		|getMinutes() == minutes
+	 * @param hours
 	 * @param minutes
 	 */
-	
-	void setMinutes(int minutes) {
-		minutesSinceMidnight = getHours()*60 + minutes;
+	public TimeOfDay(int hours, int minutes) {
+		minutesSinceMidnight = hours * 60 + minutes;
 	}
+	
 }
--- timeofday/src/timeofday/TimeOfDayTest.java
+++ timeofday/src/timeofday/TimeOfDayTest.java
@@ -8,13 +8,9 @@ class TimeOfDayTest {
 	
 	@Test
 	void test() {
-		TimeOfDay myTimeOfDay = new TimeOfDay();
-		assert myTimeOfDay.getHours() == 0;
-		assert myTimeOfDay.getMinutes() == 0;
-		myTimeOfDay.setHours(12);
-		myTimeOfDay.setMinutes(47);
+		TimeOfDay myTimeOfDay = new TimeOfDay(12,45);
 		assert myTimeOfDay.getHours() == 12;
-		assert myTimeOfDay.getMinutes() == 47;
+		assert myTimeOfDay.getMinutes() == 45;
 	}
 
 }
